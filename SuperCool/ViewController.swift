9ba35301f8a811629a686d9ed07ab13cc4caeb5e
Tron Knik <tron.knik@gmail.com>
2016-02-06 23:24:12 -0800

    Added for the first time

--- SuperCool/ViewController.swift
+++ SuperCool/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cool_logo: UIImageView!
+    
+    @IBOutlet weak var cool_bg: UIImageView!
+    
+    @IBOutlet weak var uncoolButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func makeMeNotSoUncool(sender: AnyObject) {
+        cool_logo.hidden = false
+        cool_bg.hidden = false
+        uncoolButton.hidden = true
+    }
 
 }
 
